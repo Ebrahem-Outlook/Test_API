9a6efd8a30d580b82286369c2bfe5500e24d265e
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-01-27 19:14:05 +0200

    V_1 Test-API

---
+++ Test_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_API.Models;
+
+namespace Test_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base (options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Test_API/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Test_API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
